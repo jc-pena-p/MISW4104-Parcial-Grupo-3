2911b8bba0d3379851668f99166acf0e06416db0
Juan <jc.pena@uniandes.edu.co>
2025-04-25 23:59:59 -0500

    Create routes and app module

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { CafeModule } from './cafe/cafe.module';
+
+@NgModule({
+  declarations: [],
+  imports: [BrowserModule, CafeModule],
+  providers: [],
+  bootstrap: [],
+})
+export class AppModule {}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { CafeComponent } from './cafe/cafe.component';
+
+export const routes: Routes = [{ path: '', component: CafeComponent }];
